31b3a0ee2eb7865c9ddbd98fdc25ebc463e84456
beardman <volodymyr.r@unitedtech.ai>
2022-06-24 11:38:21 +0300

    fix port

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 "use strict"
 
 const express = require("express")
-
 const app = express()
+const port = process.env.PORT || 80
 
 app.get("/", async (req, res) => {
     res.send("Hello")
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Started")
 })
