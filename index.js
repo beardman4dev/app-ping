19c9cce9824992f44f7286355c496e51d51e1beb
beardman <volodymyr.r@unitedtech.ai>
2022-06-24 11:11:47 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const express = require("express")
+
+const app = express()
+
+app.get("/", async (req, res) => {
+    res.send("Hello")
+})
+
+app.listen(3000, () => {
+    console.log("Started")
+})
